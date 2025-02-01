67986a1680d3ab62f68b2e3f6e4a5256396ece2c
quentin-fouilleul <fouilleul.quentin@outlook.fr>
2025-02-01 20:49:26 +0100

    0.01.0

---
+++ src/main/java/fr/quentin/learning/datagen/language/ModEnglishLanguageProvider.java
@@ -0,0 +1,18 @@
+package fr.quentin.learning.datagen.language;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModEnglishLanguageProvider extends FabricLanguageProvider {
+    public ModEnglishLanguageProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, "en_us", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(RegistryWrapper.WrapperLookup wrapperLookup, TranslationBuilder translationBuilder) {
+
+    }
+}
