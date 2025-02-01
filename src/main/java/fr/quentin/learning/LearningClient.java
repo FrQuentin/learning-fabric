c1d7b263fa7b5580e59a14ea6659ff0e1decdb8d
quentin-fouilleul <fouilleul.quentin@outlook.fr>
2025-02-01 20:16:26 +0100

    0.00.0

---
+++ src/main/java/fr/quentin/learning/LearningClient.java
@@ -0,0 +1,10 @@
+package fr.quentin.learning;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
